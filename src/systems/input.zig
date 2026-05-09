4cccb2a3d2dd700a4a3e55dd7e3efee06ae54026
Paul Cockrell <paulcockrell@gmail.com>
2026-05-09 17:00:34 +0100

    Fix player jump system

--- src/systems/input.zig
+++ src/systems/input.zig
@@ -7,18 +7,18 @@ const JUMP_FORCE: f32 = -250.0;
 pub fn system(world: *ecs.World) void {
     if (!raylib.isKeyPressed(raylib.KeyboardKey.space)) return;
 
-    ecs.Query.players(world, {}, spaceInput);
+    ecs.Query.players(world, {}, playerInput);
 }
 
-fn spaceInput(
+fn playerInput(
     _: void,
     ent: ecs.Entity,
-    _: *ecs.Position,
-    vel: *ecs.Velocity,
-    _: *ecs.Dimension,
+    pos: *ecs.Position,
+    _: *ecs.Velocity,
+    dim: *ecs.Dimension,
     world: *ecs.World,
 ) void {
-    if (vel.dy < 0) return; // already in jump
+    if (pos.y + dim.height < ecs.groundY(world)) return; // already in jump
 
     world.jump_intents.put(ent, .{ .force = JUMP_FORCE }) catch |err| {
         std.debug.print("Entity jump intent failed {}\n", .{err});
