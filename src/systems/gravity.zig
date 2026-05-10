7054e01f32d1a38b066eba4c2cda6d8115bbc937
Paul Cockrell <paulcockrell@gmail.com>
2026-05-10 09:48:20 +0100

    Fix gravity bug

--- src/systems/gravity.zig
+++ src/systems/gravity.zig
@@ -1,25 +1,24 @@
 const ecs = @import("../ecs.zig");
 const std = @import("std");
 
-const GRAVITY: f32 = 400.0;
+const GRAVITY: f32 = 500.0;
 const MAX_FALL_SPEED: f32 = 400.0;
 
 pub fn system(world: *ecs.World, delta: f32) void {
-    ecs.Query.players(world, delta, applyGravity);
+    ecs.Query.players(world, delta, applyPlayerGravity);
 }
 
-fn applyGravity(
+fn applyPlayerGravity(
     delta: f32,
     _: ecs.Entity,
     pos: *ecs.Position,
     vel: *ecs.Velocity,
-    _: *ecs.Dimension,
+    dim: *ecs.Dimension,
     world: *ecs.World,
 ) void {
-    const is_grounded = pos.y >= ecs.groundY(world) and vel.dy == 0;
+    const is_grounded = pos.y + dim.height >= ecs.groundY(world) and vel.dy == 0;
 
     if (is_grounded) {
-        pos.y = ecs.groundY(world);
         vel.dy = 0;
     } else {
         if (vel.dy > 0) {
