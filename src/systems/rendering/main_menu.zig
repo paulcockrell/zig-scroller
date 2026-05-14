8f10260131955256cb53425c9456f5286fe2791c
Paul Cockrell <paulcockrell@gmail.com>
2026-05-14 21:08:46 +0100

    Style main menu

--- src/systems/rendering/main_menu.zig
+++ src/systems/rendering/main_menu.zig
@@ -9,18 +9,62 @@ pub fn system(world: *ecs.World, delta: f32) void {
 }
 
 fn drawText(world: *ecs.World) void {
-    raylib.drawText(
-        raylib.textFormat("Zig Scroller", .{}),
-        @divFloor(world.screen_width, 2) - 80,
-        @divFloor(world.screen_height, 2) - 20,
-        20,
-        raylib.Color.lime,
+    var text = raylib.textFormat("Zig Scroller", .{});
+    var y = @divFloor(world.screen_height, 2) - 48;
+    drawCenteredText(
+        world,
+        text,
+        48,
+        y,
+        raylib.Color.green,
     );
-    raylib.drawText(
-        raylib.textFormat("press space to start", .{}),
-        @divFloor(world.screen_width, 2) - 120,
-        @divFloor(world.screen_height, 2) - 0,
-        16,
+
+    text = raylib.textFormat("press space to start", .{});
+    y = @divFloor(world.screen_height, 2) + 24;
+    drawCenteredText(
+        world,
+        text,
+        18,
+        y,
         raylib.Color.white,
+    );
+
+    text = raylib.textFormat("press c for credits", .{});
+    y = @divFloor(world.screen_height, 2) + 44;
+    drawCenteredText(
+        world,
+        text,
+        18,
+        y,
+        raylib.Color.yellow,
+    );
+
+    text = raylib.textFormat("press esc to exit", .{});
+    y = @divFloor(world.screen_height, 2) + 64;
+    drawCenteredText(
+        world,
+        text,
+        18,
+        y,
+        raylib.Color.red,
+    );
+}
+
+fn drawCenteredText(
+    world: *ecs.World,
+    text: [:0]const u8,
+    font_size: i32,
+    y: i32,
+    colour: raylib.Color,
+) void {
+    const text_width = raylib.measureText(text, font_size);
+    const x: i32 = @divFloor(world.screen_width - text_width, 2);
+
+    raylib.drawText(
+        text,
+        x,
+        y,
+        font_size,
+        colour,
     );
 }
