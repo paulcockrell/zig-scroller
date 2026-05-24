4028053b7e7ba76b010ff711cd8288d190ba14d9
Paul Cockrell <paulcockrell@gmail.com>
2026-05-24 15:12:16 +0100

    Fix ring sprites

--- src/entities/ring.zig
+++ src/entities/ring.zig
@@ -1,14 +1,14 @@
 const ecs = @import("../ecs.zig");
 
-const WIDTH: f32 = 16.0;
-const HEIGHT: f32 = 16.0;
-const FRAME_COUNT: i32 = 16;
+const WIDTH: f32 = 20.0;
+const HEIGHT: f32 = 20.0;
+const FRAME_COUNT: i32 = 7;
 
 pub fn spawn(world: *ecs.World) !void {
     const ent = world.ecs.createEntity();
     const x = @as(f32, @floatFromInt(world.game.screen_width + world.game.rng(0, 500)));
     const y = world.game.groundY() - HEIGHT;
-    const frame_duration: f32 = 1.0 / 12.0;
+    const frame_duration: f32 = 1.0 / 14.0;
 
     try world.ecs.rings.put(
         ent,
